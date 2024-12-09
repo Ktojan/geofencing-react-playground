727a46f5ef43ce114afd454461b2316cdfb9eefd
Andrey <karpovich.andrey@gmail.com>
2024-12-09 16:41:00 +0200

    structure optimization + color indicators for areas

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -19,7 +19,8 @@ import {
 import { Alert } from "@material-ui/lab";
 import GeoUtil from "../util/GeoUtil";
 import { MAPBOX_API_KEY } from "../../env";
-import { Position } from "@turf/turf";
+import markersFromGeojson from '../public/geofiles/markers.geo.json'
+import areasFromGeojson from '../public/geofiles/initial-areas.geo.json'
 
 function Alert2(props) {
   return <Alert elevation={6} variant="filled" {...props} />;
@@ -28,120 +29,16 @@ function Alert2(props) {
 // --------------  TABLE ---------------
 function createData(
   subject: string,
-  area: string,
+  area: Array<any>,
   time: number, //todo Date-time
   isInside: boolean,
 ) {
   return { subject, area, time, isInside };
 }
-
 // --------------  TABLE END ---------------
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiZnJvbnQta2FycCIsImEiOiJjbTJnbWQwOGQwMzl1MmtzZ2k1YmYwdzF0In0.JUG2IY5Iondl4R7lUzumxQ'; //todo dialogue to input user api-key
-
-const geojson = {
-  type: 'FeatureCollection',
-  features: [
-    {
-      type: 'Feature',
-      properties: {
-        name: 'Markio 1',
-      },
-      geometry: {
-        type: 'Point',
-        coordinates: [ 10.9711122, 50.972089 ]
-      }
-    },
-    {
-      type: 'Feature',
-      properties: {
-        name: 'Markio 2',
-      },
-      geometry: {
-        type: 'Point',
-        coordinates: [ 10.9705348, 50.973224 ]
-      }
-    },
-  ]
-};
-
-//todo relocate inside geojson or to separate file
-const mercedes1coords = [
-  [
-  10.968020388593118,
-  50.972827834411845
-],[
-  10.971137542953386,
-  50.97408440426818
-],[
-  10.970725065063903,
-  50.97445140654085
-],[
-  10.967643439854498,
-  50.9731018510395
-],[
-  10.968020388593118,
-  50.972827834411845
-]
-]
-
-const mercedes2coords: Position[] = [
-  [
-      10.970072288649476,
-      50.9733232998332
-  ],  [
-      10.97080807026299,
-      50.97266093547202
-  ],  [
-      10.972219680914321,
-      50.9729663792786
-  ],  [
-      10.971320392275572,
-      50.973848379893894
-  ],  [
-      10.970072288649476,
-      50.9733232998332
-  ]
-]
-
-const mercedes3coords: Position[] =  [
-  [
-    10.969470651174845,
-    50.97297049728169
-  ],
-  [
-    10.969790813486185,
-    50.97265936054694
-  ],
-  [
-    10.971936721803644,
-    50.97234081639877
-  ],
-  [
-    10.971907446045293,
-    50.97251678794342
-  ],
-  [
-    10.971089182791872,
-    50.97282905240701
-  ],
-  [
-    10.970585644271722,
-    50.9726438295248
-  ],
-  [
-    10.970117638674452,
-    50.97284057118674
-  ],
-  [
-    10.969741807371179,
-    50.97308532396454
-  ],
-  [
-    10.969470651174845,
-    50.97297049728169
-  ]
-]
+//todo dialogue to input user api-key
+mapboxgl.accessToken = 'pk.eyJ1IjoiZnJvbnQta2FycCIsImEiOiJjbTJnbWQwOGQwMzl1MmtzZ2k1YmYwdzF0In0.JUG2IY5Iondl4R7lUzumxQ';
 
 interface AppState {
   map?: Map;
@@ -150,7 +47,8 @@ interface AppState {
   showAlert: boolean;
   isInsideGeofence: boolean;
   tableData: any;
-  selectedAreas: Array<{ name: string, active: boolean, coords: Array<any>}>
+  selectedAreas: Array<{ name: string, color: string, active: boolean, coords: Array<any>}>;
+  markers: Array<any>;
 }
 
 export default class App extends Component<{}, AppState> {
@@ -158,24 +56,24 @@ export default class App extends Component<{}, AppState> {
   // mapContainerRef: RefObject<HTMLDivElement> | HTMLElement | null;
 
   constructor(props) {
+    console.log('areasFromGeojson ', areasFromGeojson);
+
     super(props);
+    const mercedesLocation =  { longitude: 10.969604155309582, latitude: 50.97299477896706 };
     this.mapContainerRef = React.createRef();
-    const latitude = mercedes2coords[0][1];
-    const longitude = mercedes2coords[0][0];
     this.state = {
       isInsideGeofence: false,
       showAlert: false,
       currentCheckCoords: { object: '', longitude: [], latitude: [] },
-      location: {
-        latitude,
-        longitude
-      },
+      location: { ...mercedesLocation },
       tableData: [],
-      selectedAreas: [ 
-        {name: 'Area1 green', active: true, coords: mercedes1coords},
-        {name: 'Area2 blue', active: true, coords: mercedes2coords},
-        {name: 'Area3 violet', active: false, coords: mercedes3coords}
-      ]
+      selectedAreas: areasFromGeojson.features.map(feature => ({
+         name: feature.properties.name,
+         color: feature.properties.color,
+         active: true,
+         coords: feature.geometry.coordinates})
+      ),
+      markers: []
     };
   }
 
@@ -183,113 +81,134 @@ export default class App extends Component<{}, AppState> {
     this.initMap();
   }
 
-  checkIntersection = () => {
-    let { currentCheckCoords, selectedAreas } = { ...this.state };
+  validateObjectGeofencing = (markerName: string) => {
+    let { currentCheckCoords, selectedAreas, markers } = { ...this.state };
     let geoCoordinates = [];
     selectedAreas
       .filter(area => area.active)
       .forEach(area => {
-        const addedZone = area.coords.map(el => ({
+        const addedZone = area.coords[0].map(el => ({
           "longitude": el[0],
           "latitude": el[1],
         }))
         geoCoordinates = geoCoordinates.concat(addedZone)
      });  
-    console.log(geoCoordinates);
     let isInsideGeofence = GeoUtil.pointInPolygon(geoCoordinates, currentCheckCoords);
+    const markio = markers.find(m => m.name == markerName);
+    markio.classList[isInsideGeofence ? 'remove' : 'add']('invalidated');
     this.setState({ showAlert: true, isInsideGeofence });
     return isInsideGeofence;
   };
 
   initMap() {
     mapboxgl.accessToken = MAPBOX_API_KEY;
-    const { location, currentCheckCoords, tableData, selectedAreas } = { ...this.state };
+    const { location, markers, tableData } = { ...this.state };
     const map = new mapboxgl.Map({
       container: this.mapContainerRef.current,
       style: "mapbox://styles/mapbox/streets-v11",
       center: [location.longitude, location.latitude],
+      pitch: 65,
       zoom: 16,
     });
     const scale = new mapboxgl.ScaleControl({
       maxWidth: 80,
-      unit: "imperial",
+      unit: "metric",
     });
     map.addControl(scale);
-    scale.setUnit("imperial");
-    
+
     map.on("load", () => {
-      map.addSource("geofence2", {
-        type: "geojson",
-        data: {
-          type: "Feature",
-          properties: [],
-          geometry: {
-            type: "Polygon",
-            coordinates: [mercedes2coords],
-          },
-        },
-      });
-      map.addLayer({
-        id: "geofence2",
-        type: "fill",
-        source: "geofence2",
-        layout: {},
-        paint: {
-         'fill-color': '#0080ff',
-          'fill-opacity': 0.6
-        },
-      });
-      map.addSource("geofence1", {
-        type: "geojson",
-        data: {
-          type: "Feature",
-          properties: [],
-          geometry: {
-            type: "Polygon",
-            coordinates: [mercedes1coords],
-          },
-        },
-      })
-      map.addLayer({
-        id: "geofence1",
-        type: "fill",
-        source: "geofence1",
-        layout: {},
-        paint: {
-         'fill-color': 'lightgreen',
-          'fill-opacity': 0.6
-        },
-      })
-      map.addSource("geofence3", {
-        type: "geojson",
-        data: {
-          type: "Feature",
-          properties: [],
-          geometry: {
-            type: "Polygon",
-            coordinates: [mercedes3coords],
-          },
-        },
-      })
-      map.addLayer({
-        id: "geofence3",
-        type: "fill",
-        source: "geofence3",
-        layout: {},
-        paint: {
-         'fill-color': 'indigo',
-          'fill-opacity': 0.5
-        },
-      })
+      this.add3DLayer(map);
+      
+      this.addAreasToMap(map);  
      
-    
-    for (const marker of geojson.features) {
+      if (!markersFromGeojson || markersFromGeojson.features?.length === 0) return;
+      this.addMarkersToMap(map);
+
+  });
+    this.setState({ map });   
+  }
+
+  add3DLayer(map) {
+    const labelLayerId = map.getStyle().layers.find((layer) => layer.type === 'symbol' && layer.layout['text-field']).id;
+    map.addLayer(
+      {
+        id: 'add-3d-buildings',
+        source: 'composite',
+        'source-layer': 'building',
+        filter: ['==', 'extrude', 'true'],
+        type: 'fill-extrusion',
+        minzoom: 15,
+        paint: {
+          'fill-extrusion-color': '#aaa',
+          'fill-extrusion-height': [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            15,
+            0,
+            15.05,
+            ['get', 'height']
+          ],
+          'fill-extrusion-base': [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            15,
+            0,
+            15.05,
+            ['get', 'min_height']
+          ],
+          'fill-extrusion-opacity': 0.7
+        }
+      },
+      labelLayerId
+    );
+  }
+
+  addAreasToMap(map) {
+    areasFromGeojson.features.forEach(el => {
+      map.addSource(el.properties.name, {
+        type: "geojson",
+        data: {
+          type: "Feature",
+          properties: [],
+          geometry: {
+            type: "Polygon",
+            coordinates: el.geometry.coordinates,
+          },
+        },
+      });
+      map.addLayer({
+        id: el.properties.name,
+        type: "fill",
+        source: el.properties.name,
+        layout: {},
+        paint: {
+          'fill-color': el.properties.color,
+          'fill-opacity': 0.6
+        },
+      }).on('click', el.properties.name, (e) => {
+      new mapboxgl.Popup({ closeOnClick: true, closeOnMove: true })
+        .setLngLat(e.lngLat)
+        .setHTML(`<div style="cursor: pointer;">
+          <h2 style="color: saddlebrown;">Area: ${el.properties.name}</h2>
+          <b>color: ${el.properties.color}</b><br>
+          <b>number of points: ${el.geometry.coordinates[0].length - 1}</b></div>
+          `) // first point == last point
+        .addTo(map);
+      })
+    })
+  }
+
+  addMarkersToMap(map) {
+    const { markers, tableData, selectedAreas } = { ...this.state };
+    for (const marker of markersFromGeojson.features) {
       const el = document.createElement('div');
       const p = document.createElement('p');
       p.innerText = marker.properties.name;
       el.className = 'mariomarker';
       el.appendChild(p);
-      new mapboxgl.Marker({ element: el, draggable: true })
+      const Markio = new mapboxgl.Marker({ element: el, draggable: true })
         .setLngLat(mapboxgl.LngLat.convert(marker.geometry.coordinates as [number, number]))
         .setPopup(new mapboxgl.Popup().setHTML(`<h1>${marker.properties.name}</h1><p>`))
         .on('dragend', (event) => {
@@ -297,22 +216,27 @@ export default class App extends Component<{}, AppState> {
           this.setState({
             currentCheckCoords: { name: marker.properties.name, latitude: lngLat.lat, longitude: lngLat.lng},
           });
-          const areas = selectedAreas.filter(a => a.active).reduce((acc, el)=> acc += el.name + ', ', '');
+          // const areas = selectedAreas.filter(a => a.active).reduce((acc, el)=> acc += el.name + ', ', '');
+          const areas = selectedAreas.filter(a => a.active);
+          console.log(areas);
           const newTableRecord = createData(
             marker.properties.name,
             areas,
             Date.now(),
-            this.checkIntersection()
+            this.validateObjectGeofencing(marker.properties.name)
           );         
-          tableData.push(newTableRecord);
+          tableData.unshift(newTableRecord);
           this.setState({
             tableData
           });
         }).addTo(map);
+        markers.push({ 
+          name: marker.properties.name,
+          mapObject: Markio, classList:
+          Markio.getElement().classList, valid: true 
+        })
     }
-  });
-
-    this.setState({ map });   
+    this.setState({ markers });   
   }
 
   render() {
@@ -339,18 +263,20 @@ export default class App extends Component<{}, AppState> {
               name="areas"
             >
               { selectedAreas.map((area, i) => (
-              <FormControlLabel              
-                key={i}
-                value={area.name}
-                label={area.name}
-                control={< Checkbox 
-                  checked={area.active}
-                  onChange={(e) => {
-                    selectedAreas.find(el => el.name == area.name).active = e.target['checked'];
-                    this.setState({ selectedAreas });
-                  }}
-                />}
-              />
+              <div style={{position: 'relative'}} key={area.name + i}>
+                <FormControlLabel
+                  key={i}
+                  value={area.name}
+                  label={area.name}
+                  control={<Checkbox
+                    checked={area.active}
+                    onChange={(e) => {
+                      selectedAreas.find(el => el.name == area.name).active = e.target['checked'];
+                      this.setState({ selectedAreas });
+                    } } />}                    
+                  />
+                  <span className="area-color-indicator checkbox-indicator" style={{backgroundColor: area.color}} key={area.color}></span>
+                </div>
               ))}
             </RadioGroup>
       </FormControl>
@@ -360,6 +286,7 @@ export default class App extends Component<{}, AppState> {
       <Table aria-label="simple table">
         <TableHead>
           <TableRow>
+            <TableCell>№</TableCell>
             <TableCell>Subject</TableCell>
             <TableCell>Active areas</TableCell>
             <TableCell>Time&nbsp;(Date.now)</TableCell>
@@ -367,10 +294,14 @@ export default class App extends Component<{}, AppState> {
           </TableRow>
         </TableHead>
         <TableBody>
-          {tableData.map((row) => (
+          {tableData.map((row, i) => (
             <TableRow key={row.time}>
+              <TableCell scope="row"> {tableData.length - i} </TableCell>
               <TableCell scope="row"> {row.subject} </TableCell>
-              <TableCell scope="row">{row.area}</TableCell>
+              <TableCell scope="row">{
+                  row.area.map(area => {
+                    return (<span className="area-color-indicator tablecell-indicator" style={{backgroundColor: area.color}} key={area.color}></span>)
+              })}</TableCell>
               <TableCell scope="row">{row.time}</TableCell>
               <TableCell scope="row" className={row.isInside ? 'green' : 'red'}>
                 <b>{row.isInside ? 'YES' : 'NO'}</b></TableCell>
