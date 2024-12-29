f4040b45740760c030a189edef93bba379b773ad
Andrey <karpovich.andrey@gmail.com>
2024-12-29 21:24:17 +0200

    function for markers coords + marker icon

--- src/util/Functions.ts
+++ src/util/Functions.ts
@@ -2,3 +2,14 @@ export function convertCoordsToLatLng({ ...geoJsonObject}) {
     geoJsonObject.geometry.coordinates[0] = geoJsonObject.geometry.coordinates[0].map(pair => pair.reverse())
     return geoJsonObject;
 }
+
+export function calculateMarkerCoordsForObject(geoJsonObject) {
+    const coords = geoJsonObject.geometry.coordinates[0];
+    const markerCoords = coords.reduce((res, curr, i) => {
+        {
+            res[0] += curr[0]; res[1] += curr[1];
+            return res;            
+        } 
+    }, [0, 0]).map(el => el/coords.length);
+    return markerCoords;
+}
