1ffc3e4e2974f3384fa46f6b7bbd61977f3d3e57
Andrey <karpovich.andrey@gmail.com>
2025-01-09 19:31:38 +0200

    zoom on click function

--- src/components/Markers_Points.tsx
+++ src/components/Markers_Points.tsx
@@ -1,5 +1,5 @@
 import { useMap } from 'react-leaflet';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import * as L from "leaflet";
 import type { Map } from 'leaflet';
 import { FeatureGroup, Popup, Marker } from 'react-leaflet'; //https://react-leaflet.js.org/
@@ -14,7 +14,11 @@ export default function MarkersPoints() {
     initialPoints.forEach(m => {
       const mar: any = L.marker(m.coords, { icon: pointicon});
       mar.bindTooltip(m.label, { permanent: true, opacity: 1, direction: 'auto' }).addTo(mapInstance);
-    })
+    });
+    // zoom to clicked point on map on right-button click
+    mapInstance.on('contextmenu', (e) => {
+      if (e.latlng) mapInstance.flyTo(e.latlng, 14);
+    } )
   }, [])
 
   return <FeatureGroup>
